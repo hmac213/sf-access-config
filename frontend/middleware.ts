81fd648bfb518aeb17ffb8a9ce743eee6ba5c981
Henry McNamara <hmac213@gmail.com>
2025-04-05 21:48:13 -0700

    debug statements

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  console.log('running middleware');
   // Check for your auth token, cookie, or session here
   const token = req.cookies.get('session-token');
+  console.log(token);
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
